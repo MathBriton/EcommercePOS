92f3866a8219ce4801a9973de14edbe160982d9f
Brito <mathhbrito@gmail.com>
2025-03-20 14:31:19 -0300

    OrderService

--- src/Infraestructure/obj/Debug/net9.0/Infraestructure.AssemblyInfo.cs
+++ src/Infraestructure/obj/Debug/net9.0/Infraestructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a7474b4b89f17b6afa5b7fe29401662c9e023a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bb96e36c0a42cde54b91991fd158a473c9861f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
